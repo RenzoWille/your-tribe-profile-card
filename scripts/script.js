15936a5785483ec8e7ed80f7b599ab5c2a6fae03
RenzoWille <renzo.wille@hva.nl>
2024-09-02 12:41:04 +0200

    versie 2

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 const expandButton = document.querySelector('header button')
-expandButton.addEventListener('click', expand)
+expandButton.addEventListener('click', expand) dit is de command voor de button om op te kunnen klikken
 
 function expand () {
-  document.body.classList.toggle('expand')
+  document.body.classList.toggle('expand') dit is de command voor het visitekaartje om groter te worden na het klikken op de button
 }
